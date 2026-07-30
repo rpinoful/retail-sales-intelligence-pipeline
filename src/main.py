0be18343317ef67626e2d09fb9e4e8909a3abb07
rpinoful <rapsful@gmail.com>
2026-07-30 11:26:34 -0300

    adding files changes to start the project

--- src/main.py
+++ src/main.py
@@ -1,16 +1,17 @@
+from pprint import pprint
+
 from extract import read_excel
-
-
-
-
-
-
 
 route = 'data/raw/excel/store_operations.xlsx'
 dataframes = read_excel(route)
 
-for df_name,df_content in dataframes.items():
-    print(f"The dataframe name is{df_name}\n")
-    print(df_content.shape)
+
+
+df_store = dataframes['stores']
+df_online_empty = (df_store['state'].isna()) &  (df_store['channel']=='Online')
+
+print(df_store)
+print(df_online_empty)
+
 
 
